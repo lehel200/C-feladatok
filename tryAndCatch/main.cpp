c2f649f301b9585585e164654c1b0998e2e1efbe
lehel200 <62396533+lehel200@users.noreply.github.com>
2022-05-23 12:27:53 +0200

    Update main.cpp
    két külön eset van, egy vektoros és egy sima

--- tryAndCatch/main.cpp
+++ tryAndCatch/main.cpp
@@ -28,12 +28,11 @@ RESULT:
 #include <vector>
 #include <functional>
 
-#include <typeinfo>
-
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 template<class Function, class Param>
-void tryAndCatch(Function f,Param&& p) noexcept // ezt a sort modositsa, ahogy szukseges!
+void tryAndCatch( Function f, Param&& p)// ezt a sort modositsa, ahogy szukseges!
 {
     try
     {
@@ -51,6 +50,27 @@ void tryAndCatch(Function f,Param&& p) noexcept // ezt a sort modositsa, ahogy s
 
 }
 
+template<class Function, class Param>
+void tryAndCatch(Function f, std::vector<Param>&& p)// ezt a sort modositsa, ahogy szukseges!
+{
+    try
+    {
+        f(std::move(p));
+    }
+    catch (const std::invalid_argument& ia)
+    {
+        std::cout << ia.what() << std::endl;
+    }
+    catch (const std::logic_error& le) {
+        std::cout << le.what() << std::endl;
+    }
+
+    // adja meg az implementaciot!
+
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 int main() 
 {
     
@@ -59,33 +79,28 @@ int main()
         if (x == 0.0) throw std::invalid_argument("Division by zero");
         std::cout << 1.0 / x << '\n';
     };
+    auto increment = [](int& n) { ++n; };
     tryAndCatch(printSquare, 4);
     tryAndCatch(oneOverX, 4);
     tryAndCatch(oneOverX, 0);
-    
-    
-    
-    auto increment = [](int& n) { ++n; };
+
     int num = 2;
     tryAndCatch(increment, num);
     std::cout << "After increment: " << num << '\n';
+    
     
     auto printAndClear = [](std::vector<int>&& v) {
         if (v.empty()) throw std::logic_error("Vector is empty");
         for (const auto& i : v) std::cout << i << '\n';
         v.clear();
     };
-    /*
     std::vector<int> vec = { 1, 2, 3 };
-    tryAndCatch(printAndClear, std::move(vec));
     
+    tryAndCatch(printAndClear, std::move(vec));
     tryAndCatch(printAndClear, std::move(vec));
     tryAndCatch(printAndClear, std::vector<int>{4, 3, 2, 1});
 
     tryAndCatch([](int x) {std::cout << x / 2 << '\n';}, 111);
-    */
     
-    
-    
-	return 0;
+    return 0;
 };
